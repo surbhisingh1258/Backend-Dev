b5e0884e0ed3c26e5c51562304a9f401ce6d5bc0
Surbhi Singh <surbhisingh1258@gmail.com>
2026-01-16 22:52:51 +0530

    Run-Length Encoding

--- string.js
+++ string.js
@@ -4,11 +4,27 @@
 // console.log(result);
 
 //Given two strings, check if one is a rotation of another
-let str1 = "ABCD";
-let str2 = "DCBA";
+// let str1 = "ABCD";
+// let str2 = "DCBA";
 
-if (str1.split("").reverse().join("") === str2) {
-    console.log(true);
-} else {
-    console.log(false);
+// if (str1.split("").reverse().join("") === str2) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+//Run-Length Encoding 
+let str = "aaabbc";
+let result = "";
+let count = 1;
+
+for (let i = 1; i <= str.length; i++) {
+    if (str[i] === str[i - 1]) {
+        count++;
+    } else {
+        result += str[i - 1] + count;
+        count = 1;
+    }
 }
+
+console.log(result); 
