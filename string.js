1685d65ccd71ebc8c43e5472e676aa864245b158
Surbhi Singh <surbhisingh1258@gmail.com>
2026-01-16 20:17:59 +0530

    Check for Rotation

--- string.js
+++ string.js
@@ -1,3 +1,14 @@
-let sentence = "Java is fun";
-let result=sentence.split(" ").reverse().join(" ");
-console.log(result);
+//Reverse Words in a Sentence
+// let sentence = "Java is fun";
+// let result=sentence.split(" ").reverse().join(" ");
+// console.log(result);
+
+//Given two strings, check if one is a rotation of another
+let str1 = "ABCD";
+let str2 = "DCBA";
+
+if (str1.split("").reverse().join("") === str2) {
+    console.log(true);
+} else {
+    console.log(false);
+}
